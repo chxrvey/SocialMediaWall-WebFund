afc1e10fe729931b0abf5149f00a98921e46c128
chxrvey <109527161+chxrvey@users.noreply.github.com>
2023-01-03 08:24:08 +0000

    save_14
    Javascript returns values and prints array onto screen.

--- response.js
+++ response.js
@@ -1,38 +1,40 @@
+// 21-12-2022 //
 
-/* fetching API data and turnining into an array */ 
+// Fetching the data from the URL //
 
-!async function api() {
-  // let data = await fetch("https://onstipe.com/web/api/json/6980?key=efa4b57dc7fae86ec1b71c0a58adb5ef")
-  let data = await fetch ('https://pokeapi.co/api/v2/pokemon/ditto/${abilities}')
-  .then((response) => response.json())
-  .then(data => {
-      return data;
-  })
-
-  .catch(error => {
-      console.log(error);
-  });
-  console.log(data);
-  fetchdata(1)
-};
-
-// Mapping Array //
+const url = "https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0";
+// Line 3 is a constant variable that never changes called 'URL' and its directed at that link //
+fetch (url)
+// Fetching the URL variable //
+.then((info) => info.json())
+// created a new variable called info what returns a promise from the URL(Promise being the data)//
+.then((data) => {
+// Created a new data fucntion converted from the info.json that is awaitng the promise from the line above//
+  const array = data['dataseries'];
+    document.getElementById("testData").innerHTML +=
+      'timepoint: ' + array[0].timepoint + '<br>' +
+      'cloudcover: ' + array[0].cloudcover + '<br>' +
+      'transparency: ' + array[0].transparency + '<br>';
 
 
+array.forEach((data) => {
+ // created an array variable thats equal to the data function and is looking for the information within the 'dataseries' being the array name//
+   console.log(data)
+ // // For each array data, we are putting the data function in, and outputting in into the console log//
+// // => The left part denotes the input of a function and the right part the output of that function//
+// // the data should appear in the browser console one by one//
+});
 
-  // insert to table //
+});
 
-// let table = document.getElementById("mytable");
 
-// let newRow=table.insertRow(-1);
 
-// let cell1 = newRow.insertCell(0);
-// let cell2 = newRow.insertCell(1);
 
-// cell1.innerHTML = data.api;
-// cell2.innerHTML = data.stipe;
 
-// }();
+// 21-12-2022 // 
+
+// Displaying a singlur array within my HTML // 
+
 
 
 
