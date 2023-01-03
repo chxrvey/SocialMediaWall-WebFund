ee5739f22e635a9efec1f298158f5f3784eda676
chxrvey <109527161+chxrvey@users.noreply.github.com>
2023-01-03 10:28:36 +0000

    save_15
    adjusted JavaScript to create new cell per array number. Adjusted CSS to have an overflow being auto.

--- response.js
+++ response.js
@@ -2,38 +2,69 @@
 
 // Fetching the data from the URL //
 
+// const url = "https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0";
+// // Line 3 is a constant variable that never changes called 'URL' and its directed at that link //
+// fetch (url)
+// // Fetching the URL variable //
+// .then((info) => info.json())
+// // created a new variable called info what returns a promise from the URL(Promise being the data)//
+// .then((data) => {
+// // Created a new data fucntion converted from the info.json that is awaitng the promise from the line above//
+//   var array = data['dataseries'];
+//     document.getElementById("testData").innerHTML +=
+//       'timepoint: ' + array[0].timepoint + '<br>' +
+//       'cloudcover: ' + array[0].cloudcover + '<br>' +
+//       'transparency: ' + array[0].transparency + '<br>';
+
+
+// array.forEach((data) => {
+//  // created an array variable thats equal to the data function and is looking for the information within the 'dataseries' being the array name//
+//    console.log(data)
+//  // // For each array data, we are putting the data function in, and outputting in into the console log//
+// // // => The left part denotes the input of a function and the right part the output of that function//
+// // // the data should appear in the browser console one by one//
+// });
+
+// });
+
+
+
+
+
+
+// 03-01-2023 // 
+
 const url = "https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0";
-// Line 3 is a constant variable that never changes called 'URL' and its directed at that link //
-fetch (url)
-// Fetching the URL variable //
-.then((info) => info.json())
-// created a new variable called info what returns a promise from the URL(Promise being the data)//
-.then((data) => {
-// Created a new data fucntion converted from the info.json that is awaitng the promise from the line above//
-  const array = data['dataseries'];
-    document.getElementById("testData").innerHTML +=
-      'timepoint: ' + array[0].timepoint + '<br>' +
-      'cloudcover: ' + array[0].cloudcover + '<br>' +
-      'transparency: ' + array[0].transparency + '<br>';
+// Line is a constant variable that never changes called 'URL' and its directed at that link //
+fetch(url)
+  .then((info) => info.json())
+  // created a new variable called info what returns a promise from the URL(Promise being the data)//
+  .then((data) => {
+    // Created a new data fucntion converted from the info.json that is awaitng the promise from the line above//
 
+    var array = data['dataseries'];
 
-array.forEach((data) => {
- // created an array variable thats equal to the data function and is looking for the information within the 'dataseries' being the array name//
-   console.log(data)
- // // For each array data, we are putting the data function in, and outputting in into the console log//
-// // => The left part denotes the input of a function and the right part the output of that function//
-// // the data should appear in the browser console one by one//
-});
+    // Get a reference to the table element
+    var table = document.getElementById("myTable");
 
-});
+    // Loop through the array
+    for (var i = 0; i < array.length; i++) {
+      // Insert a new row
+      var row = table.insertRow(-1);
 
+      // Insert a new cell into the row
+      var cell = row.insertCell(-1);
+      cell.innerHTML =
+        'timepoint: ' + array[i].timepoint + '<br>' +
+        'cloudcover: ' + array[i].cloudcover + '<br>' +
+        'transparency: ' + array[i].transparency + '<br>'+
+        'temp2m ' + array[i].temp2m + '<br>';
 
+        
+    }
 
-
-
-// 21-12-2022 // 
-
-// Displaying a singlur array within my HTML // 
+    console.log(data);
+  });
 
 
 
